b1db8cd6a00142b0050febe49bd58dc2bf8b2d68
SoftwareHarddata <softwareharddata@gmail.com>
2021-03-13 10:55:12 +0100

    init frontend

---
+++ backend/src/main/java/com/softwareharddata/bbetter/BbetterApplication.java
@@ -0,0 +1,13 @@
+package com.softwareharddata.bbetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbetterApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/softwareharddata/bbetter/BbetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareharddata.bbetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbetterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
